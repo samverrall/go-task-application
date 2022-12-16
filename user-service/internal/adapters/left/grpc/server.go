61684ec5e830386c0abbfe3d2efbd5d9c87cd103
Sam Verrall <samvezza@hotmail.co.uk>
2022-12-16 21:40:29 +0000

    Register gRPC user server

--- user-service/internal/adapters/left/grpc/server.go
+++ user-service/internal/adapters/left/grpc/server.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"github.com/samverrall/go-task-application/logger"
+	gen "github.com/samverrall/go-task-application/user-service/internal/adapters/left/grpc/gen/proto"
 	"github.com/samverrall/go-task-application/user-service/internal/port/service/user"
 	"google.golang.org/grpc"
 )
@@ -13,6 +14,8 @@ type GRPC struct {
 	logger      logger.Logger
 	userService user.API
 	port        int
+	server      *grpc.Server
+	gen.UnimplementedUserServer
 }
 
 func New(userSvc user.API, logger logger.Logger, port int) *GRPC {
@@ -31,10 +34,18 @@ func (g *GRPC) Run() error {
 	}
 
 	grpcServer := grpc.NewServer()
+	g.server = grpcServer
+	gen.RegisterUserServer(grpcServer, g)
 	if err := grpcServer.Serve(listen); err != nil {
 		g.logger.Error("failed to serve gRPC on port: %d, error: %v", g.port, err)
 		return err
 	}
 
+	g.logger.Info("gRPC adapter listening on %d", g.port)
+
 	return nil
+}
+
+func (g *GRPC) Stop() {
+	g.server.Stop()
 }
